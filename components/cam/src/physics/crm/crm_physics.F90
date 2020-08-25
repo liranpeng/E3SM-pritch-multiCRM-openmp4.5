7e8ef4314235567ea6919d72e5b9cf712ce058a2
Liran Peng <42455273+liranpeng@users.noreply.github.com>
2020-08-25 13:45:36 -0700

    Fix bug: edit crm_physics.F90, the initialization should not only based on pcol

--- components/cam/src/physics/crm/crm_physics.F90
+++ components/cam/src/physics/crm/crm_physics.F90
@@ -535,9 +535,13 @@ subroutine crm_physics_tend(ztodt, state, tend, ptend, pbuf, cam_in, cam_out, &
    !------------------------------------------------------------------------------------------------
    call crm_state%initialize()
    call crm_rad_initialize(crm_rad)
-   call crm_input_initialize(pcols,pver)
-   call crm_output_initialize(pcols,pver)
-
+   if (ncol .eq. 1) then
+      call crm_input_initialize(1,pver)
+      call crm_output_initialize(1,pver)
+   else
+      call crm_input_initialize(pcols,pver)
+      call crm_output_initialize(pcols,pver)
+   end if
    !------------------------------------------------------------------------------------------------
    ! Set CRM orientation angle
    !------------------------------------------------------------------------------------------------
