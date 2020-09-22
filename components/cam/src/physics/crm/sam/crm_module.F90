796028d45176d5ec97ca2f63e21920977154dc13
Liran Peng <42455273+liranpeng@users.noreply.github.com>
2020-09-21 18:24:07 -0700

    Fix bugs: edit wbaraux, crm_ww, and crm_ww_inst

--- components/cam/src/physics/crm/sam/crm_module.F90
+++ components/cam/src/physics/crm/sam/crm_module.F90
@@ -190,10 +190,10 @@ subroutine crm(nx_gl_in,ny_gl_in,nz_gl_in,dx_gl_in,dy_gl_in,&
       crmnyrad = crm_ny_rad
    end if
   
-  allocate( wbaraux(ncrms, plev) )
-  allocate( crm_ww(ncrms, plev) )
-  allocate( crm_buoya(ncrms, plev) )
-  allocate( crm_ww_inst(ncrms, plev) )
+  !allocate( wbaraux(ncrms, plev) )
+  !allocate( crm_ww(ncrms, plev) )
+  !allocate( crm_buoya(ncrms, plev) )
+  !allocate( crm_ww_inst(ncrms, plev) )
 
   allocate( t00(ncrms,nz) )
   allocate( tln(ncrms,plev) )
